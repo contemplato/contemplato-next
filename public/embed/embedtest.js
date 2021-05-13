4a5a0f4177522a56ec2d80695179e0bc5c0822a3
thiago kazuya <contato@contemplato.com>
2021-05-13 17:22:14 -0300

    add style form

--- public/embed/embedtest.js
+++ public/embed/embedtest.js
@@ -42,28 +42,83 @@ function UploadCon() {
 function getEmbed() {
   let render = document.getElementById(contemplatoConfig.idAppend);
   render.innerHTML = `
-    <div>
+   <div
+      style="border-radius: 20px;
+              border: 30px;background-color: #F6F7F9; box-shadow: 0px 10px 20px 0px rgba(1, 74, 143, 0.1);
+        max-width: 360px;
+        margin: 0px auto;  "
+    >
+      <p
+        style="
+              font-size: 15px;
+              color: #345d9d;
+              line-height: 22x;
+              padding: 10px;
+              margin-top: 30px
+              "
+      >
+        Preencha os dados que nós
+        <b> entraremos em contato em menos de 24 horas. </b>
+      </p>
+      <br />
       <input
+        style=display: flex; width: 80%; background-color: white; border: 10px ;border-radius: 10px; margin-left: 10%; margin-right: 10%; padding: 5px"
         type="text"
         id="nameCon"
         placeholder="Nome"
       />
       <br />
       <input
-        type="text"
+        style="display: flex; width: 80%; background-color: white; border: 10px ;border-radius: 10px; margin-left: 10%; margin-right: 10%; padding: 5px"
+        type="email"
         id="emailCon"
         placeholder="Email"
       />
       <br />
       <input
+        style="display: flex; width: 80%; background-color: white; border: 10px ;border-radius: 10px; margin-left: 10%; margin-right: 10%; padding: 5px"
         type="text"
         id="phoneCon"
         placeholder="Telefone"
       />
       <br />
-      <input style="" type="file" id="fileCon" placeholder="extrato" />
+      <label>
+      <input
+        style="display: none; visibility: hidden; margin-left: 10%; margin-right: 10%"
+        type="file"
+        id="fileCon"
+      />
+      <img
+        src="https://img.icons8.com/ios/452/google-docs.png"
+        alt="anexo"
+        width="30"
+        style="cursor: pointer; margin-left: 60px; margin-right: 10%"
+      />
+      </label>
       <br />
-      <button onclick="UploadCon()">Enviar</button>
+      <span style="fontsize: 11px; margintop: 0px; margin-left: 10%; margin-right: 10%"> Anexar arquivo</span>
+      <button style="background-color: #ff9632;
+                        font-size: 14px;
+                        color: #ffffff;
+                        border-radius: 10px;
+                        border: 5px;
+                        padding: 5px 20px 5px 20px;
+                        margin-bottom: 20px;
+                        margin-left: 80px;
+                        cursor: pointer"
+      onclick="UploadCon()">Enviar</button>
+      <br />
+        <div
+          style="
+          font-size: 13px;
+          color: #434443;
+          text-align: center;
+          line-height: 19px;
+          "
+        >
+        Faça uma avaliação da sua cota de consórcio conosco e venda pelo melhor
+        preço!
+      </div>
     </div>
   `;
 }
